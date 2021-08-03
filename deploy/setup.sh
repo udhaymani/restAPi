e75d7d61da02b7c6baa8ce249843e7611da33ba0
udhaymani <84677633+udhaymani@users.noreply.github.com>
2021-08-03 16:33:23 +0530

    Update setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -20,7 +20,8 @@ mkdir -p $PROJECT_BASE_PATH/env
 python3 -m venv $PROJECT_BASE_PATH/env
 
 # Install python packages
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+$PROJECT_BASE_PATH/env/bin/pip install django==2.2
+$PROJECT_BASE_PATH/env/bin/pip install djangorestframework==3.9.0
 $PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
 
 # Run migrations and collectstatic
